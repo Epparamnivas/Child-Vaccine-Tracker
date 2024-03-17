917653513d9fec3ebd0e0b30296d622138b73867
EppaRamnivas <epparamnivas121@gmail.com>
2024-03-17 13:56:10 -0400

    parent login and npm  install bcrypt

--- index.js
+++ index.js
@@ -3,24 +3,111 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 var vaccineTracker = express();
+var bcrypt = require("bcrypt");
+
+// Set up the view engine to use EJS
+vaccineTracker.set('view engine', 'ejs');
+
+//converting data into json format
+vaccineTracker.use(express.json());
+vaccineTracker.use(express.urlencoded({ extended: true }))
 
 // Setup Database Connection
 const mongoose = require('mongoose');
 const { MongoClient, Double } = require('mongodb');
-mongoose.connect('mongodb://localhost:27017/vaccineTracker', {
+mongoose.connect('mongodb://localhost:27017/ChildvaccineTracker', {
     UseNewUrlParser: true,
     UseUnifiedTopology: true
 });
 
 
-// Setup Database Model
-const PDetails = mongoose.model('ParentDetails', {
-    fathername: String,
-    mothername: String,
-    childname: String,
-    childGender: String,
-    childDOB: String,
-    comment: String,
+
+//Rwgistration Details
+
+// Setup Database Model // Define Parent Schema
+const parentSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    address: String,
+    mobileNumber: String,
+    sex: String,
+    email: String,
+    username: String,
+    password: String
+});
+
+
+// Create Parent Model
+const Parent_details = mongoose.model('parentsdetails', parentSchema);
+
+// Routes
+// Parent Registration
+vaccineTracker.post('/parent_registration', async (req, res) => {
+
+    //getting details from the form
+    const registrationdata = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: req.body.address,
+        mobileNumber: req.body.mobileNumber,
+        sex: req.body.sex,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    //checking for duplicate values
+    const existingUseremail = await Parent_details.findOne({ email: registrationdata.email });
+    const existingUsername = await Parent_details.findOne({ username: registrationdata.username });
+    if (existingUseremail) {
+        return res.status(409).send("Email id already exists, please try with different Email Id");
+    } else if (existingUsername) {
+        return res.status(409).send("Username already taken, Please use a different Username")
+    } else {
+
+        //hashing the password
+        const salthassing = await bcrypt.genSalt(12);
+        console.log("registration password  ata: "+registrationdata.password);
+
+        registrationdata.password = await bcrypt.hash(registrationdata.password, salthassing);
+ 
+        console.log("registration hased ata: "+JSON.stringify(registrationdata));
+
+        //posting the details into mongo DB
+        const createdUser = await Parent_details.create(registrationdata);
+        return res.status(201).json(createdUser);
+    };
+
+    try {
+        res.status(201).send('Parent registered successfully');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+//login for parent
+vaccineTracker.post('/parentLogin', async (req, res) => {
+    try {
+        console.log("username:" + req.body.userName);
+        const userCheck = await Parent_details.find({ username: req.body.userName });
+        console.log("username userCheck:" + userCheck);
+        if (userCheck.length === 0) {
+            return res.status(401).send('Invalid User name!');
+        }
+        console.log("ispasswordmatch password:" + req.body.password + "userCheck[0].password:" + userCheck[0].password);
+        const ispasswordmatch = await bcrypt.compare(req.body.password, userCheck[0].password);
+        console.log("ispasswordmatch:" + ispasswordmatch)
+        if (ispasswordmatch) {
+            res.render("parentLoginHome");
+        } else {
+            req.send("wrong password");
+        }
+    }
+    catch {
+        res.send("Invalid Credentials");
+    }
 });
 
 //Setup Admin Database Model
@@ -64,13 +151,13 @@ vaccineTracker.get('/aboutUs', function (req, res) {
 vaccineTracker.get('/contactUs', function (req, res) {
     res.render('contactUs',);
 });
-
+vaccineTracker.get('/parent_registration', function (req, res) {
+    res.render('parent_registration',);
+});
 
 
 // Root Page Post Method (Server Response)
 vaccineTracker.post('/', [
-    
-    
 ], function (req, res) {
     const errors = validationResult(req);
     console.log(errors);
@@ -82,24 +169,21 @@ vaccineTracker.post('/', [
     }
 });
 
-vaccineTracker.post('/userConfirm', function (req, res) {
-    console.log("User Data for DB:"+orderDB);
-    var order = new Order(orderDB);
-    order.save().then(function () {
-        console.log("Order Saved Successfully !");
-        res.render('formMessage', { message: " Added Successfully!" });
-    });
+
+
+// All  Page
+vaccineTracker.get('/parentLoginHome', (req, res) => {
+    // If Session Exists, Then Access All  Page
+    if (req.session.userLoggedIn) {
+        res.render('login-parent',);
+    }
+    else {
+        // Otherwise Redirect User To Login Page
+        res.redirect('/login-parent',);
+    }
 });
 
 
-//Validations
-
-function validateRegex(input, regEx) {
-    if (regEx.test(input)) {
-        return true;
-    } else
-        return false;
-}
 
 // Logout Page
 vaccineTracker.get('/logout', (req, res) => {
@@ -107,25 +191,6 @@ vaccineTracker.get('/logout', (req, res) => {
     req.session.userLoggedIn = false;
     res.render('login', { error: "Logout Successfully! " });
 });
-
-
-// All  Page
-vaccineTracker.get('/userDetails', (req, res) => {
-    // If Session Exists, Then Access All  Page
-    if (req.session.userLoggedIn) {
-        PDetails.find({}).then((ParentDetails) => {
-            console.log(`ParentDetails: ${ParentDetails}`);
-            res.render('userDetails', {ParentDetails: ParentDetails });
-        }).catch(function (err) {
-            console.log(`Error: ${err}`);
-        });
-    }
-    else {
-        // Otherwise Redirect User To Login Page
-        res.redirect('/login');
-    }
-});
-
 
 
 // Login Page
@@ -173,28 +238,7 @@ vaccineTracker.post('/login-parent', [
     }
 });
 
-// Delete Page - Get Method
-vaccineTracker.get('/delete/:_id', (req, res) => {
-    if (req.session.userLoggedIn) {
-        var id = req.params._id;
-        Order.findByIdAndDelete({ _id: id }).then((order) => {
-            console.log(`page : ${order}`);
-            if (order) {
-                res.render('deleteMessage', { message: "Page Deleted Successfully!" });
-            }
-            else {
-                res.render('deleteMessage', { message: "Something Went Wrong. page Not Deleted!" });
-            }
-        }).catch(function (err) {
-            console.log(`Error: ${err}`);
-        });
-    }
-    else {
-        // Otherwise Redirect User To Login Page
-        res.redirect('/login');
-    }
-});
 
 // Execute Website Using Port Number for Localhost
-vaccineTracker.listen(8080);
-console.log('Website Executed Sucessfully....Open Using http://localhost:8080/');
+vaccineTracker.listen(8082);
+console.log('Website Executed Sucessfully....Open Using http://localhost:8082/');
