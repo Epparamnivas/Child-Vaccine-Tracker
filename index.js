f9e46e1654d580db474f4104aefa9af57b7ff1e8
EppaRamnivas <epparamnivas121@gmail.com>
2024-03-24 13:06:37 -0400

    npm install jsonwebtoken and mongo db union with child collection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const path = require('path');
 var vaccineTracker = express();
 var bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 // Set up the view engine to use EJS
 vaccineTracker.set('view engine', 'ejs');
@@ -12,180 +13,6 @@ vaccineTracker.set('view engine', 'ejs');
 vaccineTracker.use(express.json());
 vaccineTracker.use(express.urlencoded({ extended: true }))
 
-// Setup Database Connection
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://vaccineTracker:Vaccinetracker%40123@child-vaccination-track.oiqbkgy.mongodb.net/childvaccinetracker", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-
-
-//Registration Details for parents
-
-// Setup Database Model // Define Parent Schema
-const parentSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    address: String,
-    mobileNumber: String,
-    sex: String,
-    email: String,
-    username: String,
-    password: String
-});
-
-
-// Create Parent Model
-const Parent_details = mongoose.model('parentsdetails', parentSchema);
-
-// Setup Database Model // Define child Schema
-const childSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    dateOfBirth: Date,
-    sex: String,
-    birthplace: String,
-    parent: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'parentsdetails'
-    }
-});
-
-
-// Create child Model
-const child_details = mongoose.model('childrendetails', childSchema);
-
-
-// Routes
-// Parent Registration
-vaccineTracker.post('/parent_registration', async (req, res) => {
-
-    //getting details from the form
-    const registrationdata = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        address: req.body.address,
-        mobileNumber: req.body.mobileNumber,
-        sex: req.body.sex,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password
-    }
-
-    //checking for duplicate values
-    const existingUseremail = await Parent_details.findOne({ email: registrationdata.email });
-    const existingUsername = await Parent_details.findOne({ username: registrationdata.username });
-    if (existingUseremail) {
-        return res.status(409).send("Email id already exists, please try with different Email Id");
-    } else if (existingUsername) {
-        return res.status(409).send("Username already taken, Please use a different Username")
-    } else {
-
-        //hashing the password
-        const salthassing = await bcrypt.genSalt(12);
-        console.log("registration password  ata: " + registrationdata.password);
-
-        registrationdata.password = await bcrypt.hash(registrationdata.password, salthassing);
-
-        console.log("registration hased ata: " + JSON.stringify(registrationdata));
-
-        //posting the details into mongo DB
-        const createdUser = await Parent_details.create(registrationdata);
-
-        res.render("login-parent");
-    };
-
-    // try {
-    //     res.status(201).send('Parent registered successfully');
-    // } catch (err) {
-    //     console.error(err);
-    //     res.status(500).send('Internal Server Error');
-    // }
-
-
-
-});
-//login for parent
-
-vaccineTracker.post('/parentLoginHome', async (req, res) => {
-    try {
-        const userCheck = await Parent_details.findOne({ username: req.body.userName });
-
-        if (!userCheck) {
-            return res.status(401).send('Invalid User name!');
-        }
-
-        const ispasswordmatch = await bcrypt.compare(req.body.password, userCheck.password);
-
-        if (ispasswordmatch) {
-            const registrationDetails = {
-                firstName: userCheck.firstName,
-                lastName: userCheck.lastName,
-                address: userCheck.address,
-                mobileNumber: userCheck.mobileNumber,
-                sex: userCheck.sex,
-                email: userCheck.email,
-                username: userCheck.username
-            };
-            // Set registrationDetails in session
-
-
-            // Render the parentLoginHome view with registrationDetails
-            res.render('parentLoginHome', { registrationDetails: registrationDetails });
-
-        } else {
-            res.send("Wrong password");
-        }
-    }
-    catch {
-        res.send("Invalid Credentials");
-    }
-
-});
-
-
-//adding child details
-
-// Routes
-//child registration
-vaccineTracker.post('/add_child_vaccination', async (req, res) => {
-
-    //getting details from the form
-    const childRegistrationData = {
-        firstName: req.body.childFirstName,
-        lastName: req.body.childLastName,
-        dateOfBirth: req.body.dateOfBirth,
-        sex: req.body.sex,
-        birthplace: req.body.birthplace,
-        parent: req.body.parentId
-    }
-
-    //checking for duplicate values
-    const existingUser = await child_details.findOne({
-        firstName: childRegistrationData.firstName,
-        lastName: childRegistrationData.lastName
-    });
-    console.log("existingUserLastname : " + childRegistrationData.lastName + "   existingUserfirstname :" + childRegistrationData.firstName);
-
-
-    if (existingUser) {
-        return res.status(409).send("Child's details already exist, please try with different details.");
-    } else {
-        //posting the details into mongo DB
-        const createdUser = await child_details.create(childRegistrationData);
-
-        res.render('childDetails', { childRegistrationData });
-    }
-});
-
-//Setup Admin Database Model
-const Admin = mongoose.model('Admin', {
-    uName: String,
-    pass: String
-
-});
 
 // Create Object Destructuring For Express Validator
 const { check, validationResult } = require('express-validator');
@@ -207,7 +34,208 @@ vaccineTracker.use(express.static(__dirname + '/public'));
 vaccineTracker.set('view engine', 'ejs');
 
 
-// Root Page Get Method (First time page load)
+// Setup Database Connection
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://vaccineTracker:Vaccinetracker%40123@child-vaccination-track.oiqbkgy.mongodb.net/childvaccinetracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('error', err => {
+    console.error('MongoDB connection error:', err);
+});
+
+////////////////////////////////////////////Registration Details for parents//////////////////////////////////////////
+
+// Setup Database Model // Define Parent Schema
+const parentSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    address: String,
+    mobileNumber: String,
+    sex: String,
+    email: String,
+    Spouse_firstName: String,
+    Spouse_lastName: String,
+    address: String,
+    Spouse_mobileNumber: String,
+    Spouse_Relation: String,
+    email: String,
+    username: String,
+    password: String
+});
+
+
+// Create Parent Model
+const Parent_details = mongoose.model('parentsdetails', parentSchema);
+
+
+
+// Setup Database Model 
+//////////////////////////////////////////////////////////////// Define child Schema/////////////////////////////////////////////////
+const childSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    dateOfBirth: Date,
+    sex: String,
+    birthplace: String,
+    parent_Id: { // Corrected field name
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'parentsdetails'
+    }
+});
+
+// Create child Model
+const child_details = mongoose.model('childrendetails', childSchema);
+
+// Routes
+//////////////////////////////////////// Parent Registration///////////////////////////////////////////////
+vaccineTracker.post('/parent_registration', async (req, res, next) => {
+    //getting details from the form
+    const registrationdata = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: req.body.address,
+        mobileNumber: req.body.mobileNumber,
+        sex: req.body.sex,
+        email: req.body.email,
+        Spouse_firstName: req.body.Spouse_firstName,
+        Spouse_lastName: req.body.Spouse_lastName,
+        Spouse_mobileNumber: req.body.Spouse_mobileNumber,
+        Spouse_Relation: req.body.Spouse_Relation,
+        Spouse_email: req.body.Spouse_email,
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    //checking for duplicate values
+    const existingUseremail = await Parent_details.findOne({ email: registrationdata.email });
+    const existingUsername = await Parent_details.findOne({ username: registrationdata.username });
+    if (existingUseremail) {
+        return res.status(409).send("Email id already exists, please try with different Email Id");
+    } else if (existingUsername) {
+        return res.status(409).send("Username already taken, Please use a different Username")
+    } else {
+        //hashing the password
+        const salthassing = await bcrypt.genSalt(12);
+        console.log("registration password  ata: " + registrationdata.password);
+        registrationdata.password = await bcrypt.hash(registrationdata.password, salthassing);
+
+        console.log("registration hased ata: " + JSON.stringify(registrationdata));
+
+        //posting the details into mongo DB
+        const createdUser = await Parent_details.create(registrationdata);
+        
+
+        //generating JWT and sending it to the client side
+        const token = jwt.sign(
+            { parentID: parentID },
+            process.env.JWT_SECRET || 'defaultSecret', // Use process.env.JWT_SECRET or a default value
+            { expiresIn: '3h' }
+        );
+
+        res.status(201).json({ message: "Parent Registered Successfully", token: token });
+
+        // Redirect or render appropriate view after registration
+        res.render("login-parent");
+    }
+});
+
+///////////////////////////////////////////login for parent///////////////////////////////
+
+vaccineTracker.post('/parentLoginHome', async (req, res) => {
+    try {
+        const userCheck = await Parent_details.findOne({ username: req.body.userName });
+
+        if (!userCheck) {
+            return res.status(401).send('Invalid User name!');
+        }
+
+        const ispasswordmatch = await bcrypt.compare(req.body.password, userCheck.password);
+
+        if (ispasswordmatch) {
+            const registrationDetails = {
+                firstName: userCheck.firstName,
+                lastName: userCheck.lastName,
+                address: userCheck.address,
+                mobileNumber: userCheck.mobileNumber,
+                sex: userCheck.sex,
+                email: userCheck.email,
+                username: userCheck.username,
+                _id: userCheck._id
+            };
+            // Render the parentLoginHome view with registrationDetails
+            req.session.registrationDetails = registrationDetails;
+
+            const parentID = registrationDetails._id;
+            console.log("Session before setting parentID:", req.session);
+            req.session.parentID = parentID;
+            console.log("Session after setting parentID:", req.session)
+
+            res.render('parentLoginHome', { registrationDetails: registrationDetails });
+
+        } else {
+            res.send("Wrong password");
+        }
+    }
+    catch {
+        res.send("Invalid Credentials");
+    }
+
+});
+
+
+////////////////////////////////////////////////adding child details///////////////////////////////////////
+//-------------------------------------------------------------------------------------------------------//
+
+// Routes
+//child registration
+vaccineTracker.post('/add_child_vaccination', async (req, res) => {
+    // Retrieve parent ID from session
+    const parentID = req.session.parentID;
+
+    // Ensure parent ID exists
+    if (!parentID) {
+        return res.status(400).send("Parent ID not found in session");
+    }
+
+    //getting details from the form
+    const childRegistrationData = {
+        firstName: req.body.childFirstName,
+        lastName: req.body.childLastName,
+        dateOfBirth: req.body.dateOfBirth,
+        sex: req.body.sex,
+        birthplace: req.body.birthplace,
+        parent_Id: parentID
+    }
+
+    //checking for duplicate values
+    const existingUser = await child_details.findOne({
+        firstName: childRegistrationData.firstName,
+        lastName: childRegistrationData.lastName,
+        parent_Id: parentID
+    });
+
+    if (existingUser) {
+        return res.status(409).send("Child's details already exist, please try with different details.");
+    } else {
+        //posting the details into mongo DB by Creating the child document in the child collection
+        const createdUser = await child_details.create(childRegistrationData);
+        console.log('Created Child:', createdUser.parent_Id); // Log created child document for debugging
+        
+        // Render the addChildDetails template with parent ID
+        res.render('addChildDetails', { registrationDetails: { _id: parentID } });
+    }
+});
+
+/////////////////////////////////////////////////////////Setup Admin Database Model//////////////////////////////////////////////////////
+const Admin = mongoose.model('Admin', {
+    uName: String,
+    pass: String
+});
+
+
+//////////////////////////////////////////////////////// Root Page Get Method (First time page load)/////////////////////////////////////
 vaccineTracker.get('/', function (req, res) {
     res.render('Home');
 });
@@ -241,7 +269,8 @@ vaccineTracker.post('/', [
 
 
 
-// All  Page
+//////////////////////////////////////////////////////// All  Page//////////////////////////////////////////////////////
+
 vaccineTracker.get('/parentLoginHome', (req, res) => {
     // Check for session and registrationDetails existence
     if (req.session && req.session.registrationDetails) {
@@ -253,6 +282,17 @@ vaccineTracker.get('/parentLoginHome', (req, res) => {
         res.redirect('/login-parent');
     }
 });
+
+vaccineTracker.get('/addChildDetails', (req, res) => {
+    const registrationDetails = req.session.registrationDetails;
+    if (registrationDetails) {
+        res.render('addChildDetails', { registrationDetails: registrationDetails });
+    } else {
+        // Handle case where session data is missing
+        res.redirect('/login-parent');
+    }
+});
+
 
 vaccineTracker.get('/childDetails', (req, res) => {
     if (req.session && req.session.childRegistrationData) {
@@ -266,7 +306,8 @@ vaccineTracker.get('/childDetails', (req, res) => {
 });
 
 
-// Logout Page
+//////////////////////////////////////////////////////// Logout Page//////////////////////////////////////////////////////
+
 vaccineTracker.get('/logout', (req, res) => {
     req.session.username = '';
     req.session.userLoggedIn = false;
@@ -274,7 +315,7 @@ vaccineTracker.get('/logout', (req, res) => {
 });
 
 
-// Login Page
+//////////////////////////////////////////////////////// Login Page//////////////////////////////////////////////////////
 vaccineTracker.get('/login-parent', (req, res) => {
     res.render('login-parent');
 });
@@ -285,9 +326,7 @@ vaccineTracker.get('/login-admin', (req, res) => {
     res.render('login-admin');
 });
 
-vaccineTracker.get('/addChildDetails', (req, res) => {
-    res.render('addChildDetails');
-});
+
 
 
 // Post Login-admin Page
@@ -323,7 +362,18 @@ vaccineTracker.post('/login-parent', [
     }
 });
 
+// Error handling middleware function
+// This function is called when an error occurs in the application
+// It logs the error stack trace to the console for debugging purposes
+// and sends a response with a 500 status code and a generic message to the user
+
+vaccineTracker.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+
 
 // Execute Website Using Port Number for Localhost
-vaccineTracker.listen(8084);
-console.log('Website Executed Sucessfully....Open Using http://localhost:8084/');
+vaccineTracker.listen(2704);
+console.log('Website Executed Sucessfully....Open Using http://localhost:2704/');
